bd0487b51171158fa64caee4e181e3ad93f2a9c7
Kevin Dias <kevindevdbs@gmail.com>
2026-01-30 21:07:55 -0300

    chore: update prisma version and format schema.prisma
    - Updated prisma dependency from ^6.19.2 to ^7.3.0 in package.json.
    - Reformatted schema.prisma for better readability by adjusting whitespace and alignment.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,20 @@
-
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+// 1. Cria o Pool de conexão (Isso é obrigatório no Prisma 7 com Adapter)
+const pool = new Pool({ connectionString });
+
+// 2. Passa o pool para o adaptador
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
+// 3. Inicializa o Prisma com o adaptador
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
