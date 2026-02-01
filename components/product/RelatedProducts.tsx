e205742a55c1de7f53cc94b09be831f283315d95
Kevin Dias <kevindevdbs@gmail.com>
2026-02-01 12:32:57 -0300

    feat: Corrects the import of the Product type in the RelatedProducts component.

--- components/product/RelatedProducts.tsx
+++ components/product/RelatedProducts.tsx
@@ -1,7 +1,7 @@
 
 import SectionTitle from "@/components/ui/SectionTitle";
 import ProductCard from "@/components/product/ProductCard";
-import { Product } from "@/app/prisma/client";
+import { Product } from "@/types";
 
 interface RelatedProductsProps {
   products: Product[];
